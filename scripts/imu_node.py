1ff56c5c71ee3f1427a50c2aa5c2ac919b21d3e7
Jonghee Son <zzongaone@gmail.com>
2023-01-03 18:58:35 +0900

    modified to use I2C Bus 0 & added launch file

--- scripts/imu_node.py
+++ scripts/imu_node.py
@@ -46,12 +46,12 @@ def publish_imu(timer_event):
     accel_z = read_word_2c(ACCEL_ZOUT_H) / 16384.0
     
     # Calculate a quaternion representing the orientation
-    accel = accel_x, accel_y, accel_z
+    '''accel = accel_x, accel_y, accel_z
     ref = np.array([0, 0, 1])
     acceln = accel / np.linalg.norm(accel)
     axis = np.cross(acceln, ref)
     angle = np.arccos(np.dot(acceln, ref))
-    orientation = quaternion_about_axis(angle, axis)
+    orientation = quaternion_about_axis(angle, axis)'''
 
     # Read the gyro vals
     gyro_x = read_word_2c(GYRO_XOUT_H) / 131.0
@@ -59,16 +59,16 @@ def publish_imu(timer_event):
     gyro_z = read_word_2c(GYRO_ZOUT_H) / 131.0
     
     # Load up the IMU message
-    o = imu_msg.orientation
-    o.x, o.y, o.z, o.w = orientation
+    '''o = imu_msg.orientation
+    o.x, o.y, o.z, o.w = orientation'''
 
-    imu_msg.linear_acceleration.x = accel_x
-    imu_msg.linear_acceleration.y = accel_y
-    imu_msg.linear_acceleration.z = accel_z
+    imu_msg.linear_acceleration.x = accel_x*9.8
+    imu_msg.linear_acceleration.y = accel_y*9.8
+    imu_msg.linear_acceleration.z = accel_z*9.8
 
-    imu_msg.angular_velocity.x = gyro_x
-    imu_msg.angular_velocity.y = gyro_y
-    imu_msg.angular_velocity.z = gyro_z
+    imu_msg.angular_velocity.x = gyro_x*0.0174
+    imu_msg.angular_velocity.y = gyro_y*0.0174
+    imu_msg.angular_velocity.z = gyro_z*0.0174
 
     imu_msg.header.stamp = rospy.Time.now()
 
@@ -81,7 +81,7 @@ imu_pub = None
 if __name__ == '__main__':
     rospy.init_node('imu_node')
 
-    bus = smbus.SMBus(rospy.get_param('~bus', 1))
+    bus = smbus.SMBus(rospy.get_param('~bus', 0))
     ADDR = rospy.get_param('~device_address', 0x68)
     if type(ADDR) == str:
         ADDR = int(ADDR, 16)
@@ -90,8 +90,8 @@ if __name__ == '__main__':
 
     bus.write_byte_data(ADDR, PWR_MGMT_1, 0)
 
-    temp_pub = rospy.Publisher('temperature', Temperature)
-    imu_pub = rospy.Publisher('imu/data', Imu)
+    temp_pub = rospy.Publisher('temperature', Temperature,queue_size=10)
+    imu_pub = rospy.Publisher('imu/data_raw', Imu,queue_size=10)
     imu_timer = rospy.Timer(rospy.Duration(0.02), publish_imu)
     temp_timer = rospy.Timer(rospy.Duration(10), publish_temp)
     rospy.spin()
